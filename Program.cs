0e66d1decc786aef0f19976c58194c89ca823cb3
Admin <Admin@DESKTOP-FKO6PH7>
2019-02-27 08:35:32 +0700

    a

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the A: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter the B: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("A + B = {0}", a + b);
+            Console.ReadKey();
+        }
+    }
+}
